1c4650dbef16ac4a16f2f9c35a30785750aade26
Romagnoli Mirco <romagnoli.mirco@gmail.com>
2018-02-20 11:02:35 +0100

    Fixed
    Source: https://stackoverflow.com/a/11428548

--- m.cc
+++ m.cc
@@ -2,7 +2,7 @@
 #include "dati.h"
 using namespace std;
 
-void inizializza (Settimana_t &settimana[I_ORARI][]){
+void inizializza (Settimana_t &settimana[I_ORARI][J_GIORNI]){
 	for (int i = 0 ; i < I_ORARI; i++){
 		for (int j = 0; j < J_GIORNI; j++){
 			settimana[i][j].aula_1 = 0;	
